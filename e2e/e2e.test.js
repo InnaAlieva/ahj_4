d8deced44c3b4d7e40a05af4c9f978238ee16177
InnaAlieva <alievainna123@gmail.com>
2025-11-17 22:20:21 +0300

    Update e2e.test.js

--- e2e/e2e.test.js
+++ e2e/e2e.test.js
@@ -1,8 +1,7 @@
-import puppetteer from 'puppeteer';
+import puppeteer from 'puppeteer';
 import { fork } from 'child_process';
 
-jest.setTimeout(30000); // default puppeteer timeout
-
+jest.setTimeout(30000); 
 describe('Credit Card Validator form', () => {
   let browser = null;
   let page = null;
